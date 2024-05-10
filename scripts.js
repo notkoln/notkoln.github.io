ac24ab91da9f2ffa0e0614cdd8682b7e1cf9270e
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-10 07:59:28 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-var random = Math.floor(Math.random() * 1) + 1
+var rare = Math.floor(Math.random() * 100) + 1
+var really_rare = Math.floor(Math.random() * 1000) + 1
 
-if (random === 1) {
+if (rare === 1 && really_rare !== 1) {
   document.body.style.backgroundColor = "green"
+} else if (rare !== 1 && really_rare === 1) {
+  document.body.style.backgroundColor = "red"
+} else if (rare === 1 && really_rare === 1) {
+  document.body.style.backgroundColor = "purple"
 }
