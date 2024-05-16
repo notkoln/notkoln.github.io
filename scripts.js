025b9d4338c2a8a2aec5ebb9605abb61afac151b
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-16 08:25:59 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,1 @@
-var rare = Math.floor(Math.random() * 100) + 1
-var really_rare = Math.floor(Math.random() * 1000) + 1
-
-if (rare === 1 && really_rare !== 1) {
-  document.body.style.backgroundColor = "green"
-} else if (rare !== 1 && really_rare === 1) {
-  document.body.style.backgroundColor = "red"
-} else if (rare === 1 && really_rare === 1) {
-  document.body.style.backgroundColor = "purple"
-}
+// removed the chance thing because it was broken
