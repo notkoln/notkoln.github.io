81f55bed71b2cd2a0a7b047c039ac883ab39f69f
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-17 09:53:51 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-document.body.addEventListener('keyup', event => {
-  if (event.code === "ArrowUp" && event.code === "KeyR") {
-    var all = document.body.querySelector("a")
-    all.innerText = "???"
-    for (let i = 0; i < 50; i++) {
-      setTimeout(function() {
-        all.style = `left: ${Math.floor(Math.random() * 50) + 1}%; top: ${Math.floor(Math.random() * 50) + 1}%`
-      }, 500)
-    }
-  }
-})
+
