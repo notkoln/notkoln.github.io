163d138ece0ce8dfbfa80d7371835195834d8dc6
notkoln <169077739+notkoln@users.noreply.github.com>
2024-05-17 09:48:55 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-// removed the chance thing because it was broken
+document.body.addEventListener('keyup', event => {
+  if (event.code === "ArrowUp" && event.code === "KeyR") {
+    var all = document.body.querySelector("a")
+    all.innerText = "???"
+    for (let i = 0; i < 50; i++) {
+      setTimeout(function() {
+        all.style = `left: ${Math.floor(Math.random() * 50) + 1}%; top: ${Math.floor(Math.random() * 50) + 1}%`
+      }, 500)
+    }
+  }
+})
